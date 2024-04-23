366481ce01050c70063aa27f3f5d232a26225a7c
horaiquenv <horaiquenv@gmail.com>
2024-04-23 10:40:17 -0300

    Added error message and error style

--- Regristro/js/scripts.js
+++ Regristro/js/scripts.js
@@ -1,3 +1,16 @@
-document.getElementByName(form){
+const FORM = () => {
 
+    let name = document.getElementById("nombre").value;
+    let surname = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("contraseña").value;
+    let confirm = document.getElementById("confirmar").value;
+    let country = document.getElementById("pais").value;
+    let terms = document.getElementById("check").value;
+
+    if (name!= String) {
+        alert("No es un nombre valido")
+    }
 }
+
+
